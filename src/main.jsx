1327c2073e4ce138f880e3e805219d54470f70f9
Rio_Register_Github <rionurfadilah229@gmail.com>
2025-05-06 00:16:46 +0700

    Add BrowserRouter to support routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; 
+import { BrowserRouter } from 'react-router-dom'; // ⬅️ Tambahan
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
